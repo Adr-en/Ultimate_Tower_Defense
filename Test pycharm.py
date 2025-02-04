d78bd6f7e2df1af106e922fdd9b29f78cbd5bb1c
Ethan-Sauvanet <ethan.sauvanet@efrei.net>
2025-02-04 09:22:14 +0100

    Test 3 - Starting mechanics

--- Test pycharm.py
+++ Test pycharm.py
@@ -13,7 +13,16 @@ clock.tick(60)
 test = pygame.image.load('test.png').convert() #Import une image mise dans le dossier en amount
 test = pygame.transform.smoothscale(test, (50,50)) #Changing the size
 
-x = 0
+tower = pygame.image.load('test_tower.png').convert()
+tower = pygame.transform.smoothscale(tower, (100,100))
+
+emp1 = pygame.Surface((50,50))
+emp2= pygame.Surface((50,50))
+emp1.fill('green')
+emp2.fill('green')
+
+
+pos_adrien = 0
 y = 400
 velocity = 0.2
 
@@ -22,17 +31,22 @@ while running : #Pour ouvrir la fenetre en continu
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(test, (x , y)) #Place l'image
+    screen.blit(test, (pos_adrien , y)) #Place l'image test
+    screen.blit(tower, (1300, 600))
+
+    screen.blit(emp1, (800, 600))
+    screen.blit(emp2, (800, 200))
+
     pygame.display.flip() #ou .update(x,x) rafraichis l'écran pour afficher
 
-    while x <= 1490 :
+    while pos_adrien <= 1490 :
 
-        old_rect = pygame.Rect(x, y, 50, 50)  #Store old position
-        x += velocity  # Move right
-        new_rect = pygame.Rect(x, y, 50, 50)  #New position
+        old_rect = pygame.Rect(pos_adrien, y, 50, 50)  #Store old position
+        pos_adrien += velocity  # Move right
+        new_rect = pygame.Rect(pos_adrien, y, 50, 50)  #New position
 
-        screen.fill('white', old_rect) #Clear l'ancienne position
-        screen.blit(test, (x, y)) #Donne la nouvelle position
+        screen.fill((255,255,255), old_rect) #Clear l'ancienne position
+        screen.blit(test, (pos_adrien, y)) #Donne la nouvelle position
         pygame.display.flip()
 
 pygame.quit()
