cac450b16fbb3507cbb198ed0f10bf437695be6b
Raphaël <rapha.freser@gmail.com>
2025-02-10 10:08:54 +0100

    Started the different classes for the projectiles

---
+++ Class Projectile.py
@@ -0,0 +1,15 @@
+class Arrow:
+    def __init__(self, start_position, end_position):
+        self.speed = 5
+        self.trajectory = None
+        self.effect = None
+        self.damage = 50
+        self.position = start_position
+        self.end_position = end_position
+        self.hitbox = None
+
+    def movement(self):
+
+
+
+
