da398a124f8aa62c087674e07c7710a8c6cf8b97
Ethan-Sauvanet <ethan.sauvanet@efrei.net>
2025-03-24 10:04:11 +0100

    Screen adjustments

--- Definitions/definitions_main_menu.py
+++ Definitions/definitions_main_menu.py
@@ -20,26 +20,26 @@ main_menu_background = pygame.transform.smoothscale(main_menu_background, size)
 #Initializing the start button
 start_button_image = pygame.image.load('Assets/Start_button.png')
 start_button_image = pygame.transform.smoothscale(start_button_image, size)
-start_button = pygame.Surface((200, 75))
-start_button_rect = start_button.get_rect(topleft=(705, 580))
+start_button = pygame.Surface((170, 75))
+start_button_rect = start_button.get_rect(topleft=(675, 495))
 
 
 #Initializing the option button
 option_button_image = pygame.image.load('Assets/Options_button.png')
 option_button_image = pygame.transform.smoothscale(option_button_image, size)
-option_button = pygame.Surface((200, 75))
-option_button_rect = option_button.get_rect(topleft=(705, 660))
+option_button = pygame.Surface((170, 65))
+option_button_rect = option_button.get_rect(topleft=(675, 570))
 
 
 #Initializing the credits button
 credits_button_image = pygame.image.load('Assets/Credits_button.png')
 credits_button_image = pygame.transform.smoothscale(credits_button_image, size)
-credits_button = pygame.Surface((200, 75))
-credits_button_rect = credits_button.get_rect(topleft=(705, 735))
+credits_button = pygame.Surface((170, 65))
+credits_button_rect = credits_button.get_rect(topleft=(675, 635))
 
 
 #Initializing the quit game button
 quit_button_image = pygame.image.load('Assets/Quit_button.png')
 quit_button_image = pygame.transform.smoothscale(quit_button_image, size)
-quit_button = pygame.Surface((130, 65))
-quit_button_rect = quit_button.get_rect(topleft=(1470, 0))
+quit_button = pygame.Surface((140, 70))
+quit_button_rect = quit_button.get_rect(topleft=(1380, 0))
