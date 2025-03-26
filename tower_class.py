f0fc04f37044d54f0df7fac19aec8ab32a6667f2
minousama <minh-lam.nguyen@efrei.net>
2025-03-26 14:48:29 +0100

    update of the tower class, could you check if the "while self.built" loop makes sense

--- tower_class.py
+++ tower_class.py
@@ -1,52 +1,92 @@
 import pygame as p
-import time as t
-
+import math
+import class_enemy as en
+import class_projectile as pro
 
 class Tower:
     def __init__(self, x, y):  # definition of the class
         self.screen = p.display.set_mode((1540, 800))
         self.dest = (x, y)
-        self.surf = p.image.load("Assets/default_tower.png")  # surface of the tower
+        self.surf = p.image.load("Assets/available_tower.png")  # surface of the tower
         self.tower = p.Rect(x, y, 90, 180)  # the tower itself
         self.built = False  # to know if the tower is built
         self.attack_speed = 0.5  # attack speed of the tower
-        #self.ammo =  # Raphael!!!!!!!!!!!
+        self.list_ammo = ["..."]
+        self.ammo = None
+        self.range = 0
+
+
+        while self.built:
+                """Return the furthest ennemy in range of the tower
+                    Tower is an object of type tower 
+                    element and ele are object of type Ennemy"""
+
+                tower_pos = p.math.Vector2(self.dest)
+                list_range = [] # list of enemies in range
+                for element in en.liste_ennemy: # allows us to know if there is enemies in range
+                    dist = tower_pos - element.pos
+                    dist = dist.length()
+                    if dist <= self.range:
+                        list_range.append(element)
+                    # this checks which enemy is the furthest
+                    furthest_ele = list_range[0]
+                    for elem in list_range:
+                        if furthest_ele.pos[0] < elem.pos[0]:
+                            furthest_ele = elem
+                    # now we locked the enemy
+                    while furthest_ele in list_range: # while this enemy is in range
+                        #self.ammo()
+                        list_range = []
+                        for element in en.liste_ennemy:  # we update the list to see if the enemy is still in range
+                            dist = tower_pos - element.pos
+                            dist = dist.length()
+                            if dist <= self.range:
+                                list_range.append(element)
+
+
+
+
 
     def draw(self):
         self.screen.blit(self.surf, self.dest)
-        #p.display.update()
 
+
+    """definition of every type of tower"""
     def archer(self):
-        try:
-            self.surf = p.image.load("Assets/archer_tower.png")  # where we'll have the skin of the mob
-            self.built = True
-            self.screen.blit(self.surf, self.dest)
-        except p.error as e:
-            print(e)
+        self.surf = p.image.load("Assets/archer_tower.png")  # where we'll have the skin of the mob
+        self.built = True # states that the tower is active
+        self.ammo = self.list_ammo[0]
+        self.screen.blit(self.surf, self.dest) #prints the tower
 
+    """same patern everywhere"""
     def bomber(self):
         self.surf = p.image.load("Assets/rock_tower.png")
         self.built = True
+        self.ammo = self.list_ammo[1]
         self.screen.blit(self.surf, self.dest)
 
     def slow(self):
         self.surf = p.image.load("Assets/ice_tower.png")
         self.built = True
+        self.ammo = self.list_ammo[2]
         self.screen.blit(self.surf, self.dest)
 
     def fire(self):
         self.surf = p.image.load("Assets/fire_tower.png")
         self.built = True
+        self.ammo = self.list_ammo[3]
         self.screen.blit(self.surf, self.dest)
 
     def adrien(self):
         self.surf = p.image.load("Assets/adrien_tower.png")
         self.built = True
+        self.ammo = self.list_ammo[4]
         self.screen.blit(self.surf, self.dest)
-
+    """method to delete a tower"""
     def supr(self):
-        self.surf = p.image.load("Assets/default_tower.png")
-        self.built = False
+        self.surf = p.image.load("Assets/available_tower.png")
+        self.built = False # states that there the emplacement is
+        self.ammo = None
         self.screen.blit(self.surf, self.dest)
 
 
